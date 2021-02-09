4da6c3612ab75e97879c8ce1ecf3166f2dda606d
Pusha <de24world@gmail.com>
2021-02-09 19:57:31 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import ToggleSwitch from "./ToggleSwitch";
+
+export default function App() {
+  return (
+    <div className="App">
+      <ToggleSwitch />
+    </div>
+  );
+}
